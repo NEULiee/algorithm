687c6755cfd49fe3e1775c6acebbb78827546df2
NEULiee <asdfz888@naver.com>
2021-01-25 00:12:11 +0900

    set

--- boj/14425_map/main.cpp
+++ boj/14425_map/main.cpp
@@ -1,14 +1,34 @@
 //
 //  main.cpp
-//  14425_map
+//  14425 문자열 탐색 (map)
 //
 //  Created by Ha Neul Iee on 2021/01/24.
 //
 
 #include <iostream>
+#include <string>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int N, M, cnt;
+string input;
+unordered_set<string> s;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i=0; i<N; i++) {
+        cin >> input;
+        s.insert(input);
+    }
+    for (int i=0; i<M; i++) {
+        cin >> input;
+        if (s.find(input) != s.end()) {
+            cnt++;
+        }
+    }
+    cout << cnt << "\n";
     return 0;
 }
