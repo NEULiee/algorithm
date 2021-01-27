135de4e7671fc6cdecd8be2c8a7d2678b3bbd2bc
NEULiee <asdfz888@naver.com>
2021-01-28 02:23:55 +0900

    binary search

--- boj/10816/main.cpp
+++ boj/10816/main.cpp
@@ -7,10 +7,11 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int N, M, input;
-int card[500000];
+vector<int> card;
 
 int lower_bound(int num) {
     int first = 0, last = N - 1, mid;
@@ -48,20 +49,25 @@ int main(int argc, const char * argv[]) {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     
+    int c;
     cin >> N;
     for (int i=0; i<N; i++) {
-        cin >> card[i];
+        cin >> c;
+        card.push_back(c);
     }
-    sort(card, card + N);
+    sort(card.begin(), card.end());
     cin >> M;
     for (int i=0; i<M; i++) {
         cin >> input;
+        /*
         int lower = lower_bound(input);
         int upper = upper_bound(input);
         if (upper == N-1 && card[N-1] == input) {
             upper++;
         }
         cout << upper - lower << " ";
+         */
+        cout << upper_bound(card.begin(), card.end(), input) - lower_bound(card.begin(), card.end(), input) << "\n";
     }
     cout << "\n";
     return 0;
